11bd86716730db58e6fd7077c4754da2c3e4eea5
MarcioCastilho72 <castilho72marcio@gmail.com>
2025-05-06 17:20:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,3 +24,9 @@ def extract_text():
         return jsonify({"text": full_text.strip()})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
